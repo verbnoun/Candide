cac04ec1a0e5d070ce7177010d12947e8f619092
Verb / Noun <188970+verbnoun@users.noreply.github.com>
2024-12-03 06:18:55 -0500

    voices refactor start

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ note/panning/per_key/-1-1/pitch_bend
 oscillator/waveform/global/saw/note_on
 oscillator/ring/frequency/global/20-2000/cc74
 oscillator/ring/waveform/global/triangle/note_on
-oscillator/ring/bend/global/-12-12/cc75
+oscillator/ring/bend/per_key/-12-12/pitch_bend
 
 filter/band_pass/resonance/global/0.1-2.0/cc71
 filter/band_pass/frequency/global/20-20000/cc70
@@ -31,10 +31,10 @@ amplifier/envelope/decay_time/global/0.001-0.25/cc75
 amplifier/envelope/sustain_level/per_key/0-1/pressure
 amplifier/envelope/release_time/global/0.001-3/cc72
 
-lfo/rate/tremolo_lfo/global/0.1-10/cc71
-lfo/scale/tremolo_lfo/global/0-1/cc72
-lfo/offset/tremolo_lfo/global/-1-1/cc73
-lfo/phase_offset/tremolo_lfo/global/0-1/cc74
-lfo/once/tremolo_lfo/global/0-1/cc75
-lfo/interpolate/tremolo_lfo/global/0-1/cc76
+lfo/rate/tremolo_lfo/global/0.1-10/cc102
+lfo/scale/tremolo_lfo/global/0-1/cc103
+lfo/offset/tremolo_lfo/global/-1-1/cc104
+lfo/phase_offset/tremolo_lfo/global/0-1/cc105
+lfo/once/tremolo_lfo/global/0-1/cc106
+lfo/interpolate/tremolo_lfo/global/0-1/cc107
 '''
